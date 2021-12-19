f00f41e44d0616ac8b3c6b8bed5673880fbf06df
RoshanKM <roshan.techacc88@gmail.com>
2021-12-19 13:20:20 +0530

    Project repository set-up

---
+++ src/main/java/com/rkmgithubacc/hirewheels/HirewheelsH2Application.java
@@ -0,0 +1,13 @@
+package com.rkmgithubacc.hirewheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirewheelsH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirewheelsH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkmgithubacc/hirewheels/HirewheelsH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkmgithubacc.hirewheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirewheelsH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
